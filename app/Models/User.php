6904d0a3059d5c1885ec8d6900e846bc0bc583a8
raycollamat <raycollamat11@gmail.com>
2023-07-11 13:06:13 +0800

    add> ERD models & tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Orchid\Filters\Types\Like;
 use Orchid\Filters\Types\Where;
 use Orchid\Filters\Types\WhereDateStartEnd;
@@ -9,6 +10,14 @@ use Orchid\Platform\Models\User as Authenticatable;
 
 class User extends Authenticatable
 {
+    use HasFactory;
+
+    // relationship to CartItem
+    public function cart_item()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
