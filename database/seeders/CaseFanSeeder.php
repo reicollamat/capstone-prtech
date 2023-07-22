d1dfea3c2c639e5e6978ca6cf3c60f3c3398f29a
raycollamat <raycollamat11@gmail.com>
2023-07-22 19:04:34 +0800

    compress.3

--- database/seeders/CaseFanSeeder.php
+++ database/seeders/CaseFanSeeder.php
@@ -21,7 +21,7 @@ class CaseFanSeeder extends Seeder
         $dataset = json_decode($json);
 
         foreach (array_slice($dataset, 0, 100) as $key => $value) {
-            $image = 'img/components/casefan/casefan (' . fake()->numberBetween(1, 3) . ').png';
+            $image = 'img/components/casefan/casefan (' . fake()->numberBetween(1, 2) . ').png';
             $condition = fake()->randomElement(['brand_new', 'used']);
             if (!empty($value->price)) {
                 $product = Product::create([
