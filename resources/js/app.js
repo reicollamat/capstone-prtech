85d9ec31d16ff3eec21c6ae38d4f68f5152eadb3
richmondb <richmond.billones@gmail.com>
2023-10-10 00:52:55 +0800

    remove alpine, livewire loads alpine already

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,10 +6,10 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-// Disable alpine plugin for now for livewire
-import Alpine from 'alpinejs';
-//
-window.Alpine = Alpine;
-//
-Alpine.start();
+// // Disable alpine plugin for now for livewire
+// import Alpine from 'alpinejs';
+// //
+// window.Alpine = Alpine;
+// //
+// Alpine.start();
 
