df3e82f1da220dd41c37dcbf1fccf004bf71590a
richmondb <richmond.billones@gmail.com>
2023-10-22 00:51:52 +0800

    added alias for Helper class

--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Helper' => App\Helper\Helper::class,
     ])->toArray(),
 
 ];
