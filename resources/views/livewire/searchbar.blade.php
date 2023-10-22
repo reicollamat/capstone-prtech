1720b1f263703e3bce873cbb7f64b20f1a8459ad
richmondb <richmond.billones@gmail.com>
2023-10-22 20:32:52 +0800

    added more ui friendly loading indicator

--- resources/views/livewire/searchbar.blade.php
+++ resources/views/livewire/searchbar.blade.php
@@ -17,11 +17,20 @@
                    aria-label="Search" aria-describedby="button-addon2">
             <button class="btn btn-outline-secondary rounded-none d-flex items-center gap-2" type="submit"
                     id="button-addon2">
-                <svg xmlns="http://www.w3.org/2000/svg" width=20 height="20" fill="currentColor"
-                     class="bi bi-search" viewBox="0 0 16 16">
-                    <path
-                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                </svg>
+                <div wire:loading.remove>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width=20 height="20" fill="currentColor"
+                             class="bi bi-search" viewBox="0 0 16 16">
+                            <path
+                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                    </div>
+                </div>
+                <div wire:loading>
+                    <div class="spinner-border spinner-border-sm" role="status">
+                        <span class="visually-hidden">Loading...</span>
+                    </div>
+                </div>
                 Search
             </button>
         </div>
