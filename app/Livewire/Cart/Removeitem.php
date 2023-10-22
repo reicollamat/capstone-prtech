601a0c8f9bdc92c1270df05bc6b706d72a5204c8
richmondb <richmond.billones@gmail.com>
2023-10-22 20:33:12 +0800

    experimental livwire remove cart item

---
+++ app/Livewire/Cart/Removeitem.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Cart;
+
+use Livewire\Component;
+
+class Removeitem extends Component
+{
+    public function render()
+    {
+        return view('livewire..cart.removeitem');
+    }
+}
