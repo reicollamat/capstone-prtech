c7b7df7db227db15ccdccf49909c156cccd5a3fe
richmondb <richmond.billones@gmail.com>
2023-10-28 21:56:26 +0800

    new buttons that doesnt need reload

---
+++ app/Livewire/Addtocart/AddToCart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Addtocart;
+
+use Livewire\Component;
+
+class AddToCart extends Component
+{
+    public function render()
+    {
+        return view('livewire..addtocart.add-to-cart');
+    }
+}
---
+++ app/Livewire/Addtowishlist/AddToWishlist.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Addtowishlist;
+
+use Livewire\Component;
+
+class AddToWishlist extends Component
+{
+    public function render()
+    {
+        return view('livewire..addtowishlist.add-to-wishlist');
+    }
+}
