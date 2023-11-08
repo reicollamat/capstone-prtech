774bc8133c8eb6759f7f0bb106a361acf37580ac
richmondb <richmond.billones@gmail.com>
2023-11-08 23:38:20 +0800

    ui cleanup

--- resources/views/livewire/seller/auth/login-page.blade.php
+++ resources/views/livewire/seller/auth/login-page.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <nav class="navbar bg-light shadow-2xl">
+    <nav class="navbar bg-light shadow-xl">
         <div class="container-fluid !justify-start md:!px-36 py-2">
             <a class="navbar-brand" href="/">
                 <img src="/img/brand/svg/logo-no-background.svg" alt="Logo" width="175" height="175"
