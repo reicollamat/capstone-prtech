ff13c3c4503133a568f9f2a71b61da41f6570b12
richmondb <richmond.billones@gmail.com>
2023-11-09 01:21:21 +0800

    ano to uhm responsive logo size

---
+++ resources/views/livewire/seller/on-boarding/form/shop-information.blade.php
@@ -0,0 +1,65 @@
+<div>
+    <nav class="navbar bg-light shadow-xl">
+        <div class="container-fluid !justify-start md:!px-36 py-2">
+            <a class="navbar-brand" href="/">
+                <div class="w-[120px] sm:w-[175px] h-auto">
+                    <img src="/img/brand/svg/logo-no-background.svg" alt="Logo" width="100%" height="100%"
+                        class="d-inline-block align-text-top" />
+                </div>
+            </a>
+            <h class="tracking-tight text-lg sm:text-2xl">Seller Registration</h>
+        </div>
+    </nav>
+    <div>
+        <div class="container-fluid !bg-red md:!px-14 py-14 ">
+            <div class="flex gap-6">
+                <div class="!bg-transparent p-2.5 py-24 pl-4">
+                    <ol class="relative text-gray-500 border-l border-gray-200 dark:border-gray-700 dark:text-gray-400">
+                        <li class="mb-10 ml-3">
+                            <span
+                                class="absolute flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
+                                <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                    <path
+                                        d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                                </svg>
+                            </span>
+                            <h5 class="font-medium leading-tight">Shop Information</h5>
+                            <p class="text-sm">Step details here</p>
+                        </li>
+                        <li class="mb-10 ml-3">
+                            <span
+                                class="absolute flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-gray-700">
+                                <svg class="w-3.5 h-3.5 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+                                    <path
+                                        d="M18 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2ZM6.5 3a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5ZM3.014 13.021l.157-.625A3.427 3.427 0 0 1 6.5 9.571a3.426 3.426 0 0 1 3.322 2.805l.159.622-6.967.023ZM16 12h-3a1 1 0 0 1 0-2h3a1 1 0 0 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Zm0-3h-3a1 1 0 1 1 0-2h3a1 1 0 1 1 0 2Z" />
+                                </svg>
+                            </span>
+                            <h5 class="font-medium leading-tight">Business Information</h5>
+                            <p class="text-sm">Step details here</p>
+                        </li>
+                        <li class="mb-10 ml-3">
+                            <span
+                                class="absolute flex items-center justify-center w-10 h-10  bg-green-200 rounded-full -left-4 ring-4 ring-white dark:ring-gray-900 dark:bg-green-900">
+                                <svg class="w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
+                                </svg>
+                            </span>
+                            <h5 class="font-medium leading-tight text-success">Success</h5>
+                            <p class="text-sm"></p>
+                        </li>
+
+                    </ol>
+
+                </div>
+                <div class="flex-1 shadow border border-solid border-gray-200 p-3">
+                    test
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
