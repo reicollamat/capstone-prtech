e735e9a6b392e794d54227ef9b1381ef844e5a5c
reicollamat <raycollamat11@gmail.com>
2023-11-09 05:12:15 +0000

    Update sidebar.blade.php

--- resources/views/layouts/seller/sidebar.blade.php
+++ resources/views/layouts/seller/sidebar.blade.php
@@ -9,64 +9,28 @@
 
   <div class="list-group list-group-flush py-3">
     <a wire:navigate class="list-group-item list-group-item-action p-3 border-0 {{ (request()->routeIs('seller_dashboard')) ? 'active' : '' }}" href="{{ route('seller_dashboard') }}">
-      <div class="row">
-        <div class="col-2">
-          <i class="bi bi-house-fill" aria-hidden="true"></i>
-        </div>
-        <div class="col">
-          Dashboard
-        </div>
-      </div>
+        <i class="bi bi-house-fill" aria-hidden="true"></i>
+        Dashboard
     </a>
     <a wire:navigate class="list-group-item list-group-item-action p-3 border-0 {{ (request()->routeIs('seller_inventory')) ? 'active' : '' }}" href="{{ route('seller_inventory') }}">
-      <div class="row">
-        <div class="col-2">
-          <i class="bi bi-database-fill" aria-hidden="true"></i>
-        </div>
-        <div class="col">
-          Product Inventory
-        </div>
-      </div>
+        <i class="bi bi-database-fill" aria-hidden="true"></i>
+        Product Inventory
     </a>
     <a class="list-group-item list-group-item-action p-3 border-0" href="#!">
-      <div class="row">
-        <div class="col-2">
-          <i class="bi bi-bar-chart-line-fill" aria-hidden="true"></i>
-        </div>
-        <div class="col">
-          Statistics
-        </div>
-      </div>
+        <i class="bi bi-bar-chart-line-fill" aria-hidden="true"></i>
+        Statistics
     </a>
     <a class="list-group-item list-group-item-action p-3 border-0" href="#!">
-      <div class="row">
-        <div class="col-2">
-          <i class="bi bi-cart-fill" aria-hidden="true"></i>
-        </div>
-        <div class="col">
-          Purchases
-        </div>
-      </div>
+        <i class="bi bi-cart-fill" aria-hidden="true"></i>
+        Purchases
     </a>
     <a class="list-group-item list-group-item-action p-3 border-0" href="#!">
-      <div class="row">
-        <div class="col-2">
-          <i class="bi bi-truck" aria-hidden="true"></i>
-        </div>
-        <div class="col">
-          Deliveries
-        </div>
-      </div>
+        <i class="bi bi-truck" aria-hidden="true"></i>
+        Deliveries
     </a>
     <a class="list-group-item list-group-item-action p-3 border-0" href="#!">
-      <div class="row">
-        <div class="col-2">
-          <i class="bi bi-person-circle" aria-hidden="true"></i>
-        </div>
-        <div class="col">
-          Profile
-        </div>
-      </div>
+        <i class="bi bi-person-circle" aria-hidden="true"></i>
+        Profile
     </a>
   </div>
 </div>
