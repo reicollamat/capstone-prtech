9affca1f8e2f12842b201c1eb1db816686be140c
reicollamat <raycollamat11@gmail.com>
2023-11-11 17:13:08 +0000

    change profile page layout

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,5 @@
-<x-app-layout>
+@extends('layouts.master_layout')
+@section('content')
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Profile') }}
@@ -26,4 +27,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
