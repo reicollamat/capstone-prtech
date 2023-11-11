b4d39c40079603dccd6efae605ddf209d1a3794c
richmondb <richmond.billones@gmail.com>
2023-11-12 00:07:00 +0800

    remove nullable info

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,34 +32,21 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'first_name' => ['required', 'string', 'max:255'],
-            'last_name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
-            'phone_number' => ['required', 'string', 'max:255'],
-            'street_address' => ['required', 'string', 'max:255'],
-            'city' => ['required', 'string', 'max:255'],
-            'postal_code' => ['required', 'string', 'max:255'],
-            'country' => ['required', 'string', 'max:255'],
+
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'first_name' => $request->first_name,
-            'last_name' => $request->last_name,
-            'phone_number' => $request->phone_number,
-            'street_address' => $request->street_address,
-            'city' => $request->city,
-            'postal_code' => $request->postal_code,
-            'country' => $request->country,
             'permissions' => [
-                "platform.index" => false, 
+                "platform.index" => false,
                 "platform.systems.roles" => false,
                 "platform.systems.users" => false,
                 "platform.systems.attachment" => false,
-            ], 
+            ],
         ]);
 
         event(new Registered($user));
