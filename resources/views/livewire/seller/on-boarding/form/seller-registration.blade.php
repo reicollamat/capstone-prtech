112341bafd7c4da698387a93ef3b0ea569a402e2
reicollamat <raycollamat11@gmail.com>
2023-11-11 19:05:12 +0000

    add modal for testing

--- resources/views/livewire/seller/on-boarding/form/seller-registration.blade.php
+++ resources/views/livewire/seller/on-boarding/form/seller-registration.blade.php
@@ -164,6 +164,32 @@
                     <div class="flex justify-end gap-3">
                         <p>{{ $user_sex }} test</p>
                         <p>{{ $userid }}</p>
+
+                    {{-- modal test --}}
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            Proceed Modal
+                        </button>
+                        
+                        <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                ...
+                                </div>
+                                <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Understood</button>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        
                         <button type="submit" class="btn btn-primary">
                             <span wire:loading.remove wire:target="RegistrationForm">
                                 Proceed
