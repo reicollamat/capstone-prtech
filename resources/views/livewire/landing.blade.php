1f85768719143e478a5721cee79d9a3ee670d2eb
richmondb <richmond.billones@gmail.com>
2023-11-12 00:07:21 +0800

    test for alpine if loaded correctly

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,6 +1,12 @@
 <div>
-    {{ $count }}
+    {{--    {{ $count }} --}}
     {{--    <livewire:shop.collections/> --}}
     {{--    {{ $collection->title }} --}}
-    @dd(Auth::user())
+    {{--    @dd(Auth::user()) --}}
+
+    <div x-data="{ count: 0 }">
+        <button x-on:click="count = count > 0 ? count-1 : count">-</button>
+        <span x-text="count"></span>
+        <button x-on:click="count++">+</button>
+    </div>
 </div>
