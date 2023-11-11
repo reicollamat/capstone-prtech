ecb819b6e8989c21a59c2092f09b124148d829b6
richmondb <richmond.billones@gmail.com>
2023-11-12 00:07:10 +0800

    new ui

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,48 +1,62 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <title> {{ $title ?? 'PR - TECH' }}</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="PR-Tech, E-commerce" name="keywords">
+    <meta content="PR-Tech is an E-commerce website that provides products for all your needs." name="description">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <style>
+        [x-cloak] {
+            visibility: hidden !important;
+            overflow: hidden !important;
+            display: none !important;
+        }
+    </style>
 
+    <!-- Favicon -->
+    <link href="{{ asset('img/icon/retechicon.ico') }}" rel="icon">
 
-        <!-- Favicon -->
-        <link href="{{ asset('img/icon/retechicon.ico') }}" rel="icon">
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
-        <!-- Google Web Fonts -->
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <!-- Font Awesome -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    {{-- This directive is used to include the Livewire styles --}}
+    @livewireStyles
 
-        <!-- Libraries Stylesheet -->
-        <link href="{{ asset('multishop/lib/animate/animate.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('multishop/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="{{ asset('multishop/css/style.css') }}" rel="stylesheet">
+</head>
 
-        <!-- Customized Bootstrap Stylesheet -->
-        <link href="{{ asset('multishop/css/style.css') }}" rel="stylesheet">
+<body>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-light dark:bg-gray-900">
-            <div class="d-none d-lg-flex">
-                <a href="/" class="text-decoration-none">
-                    <x-application-logo class="" />
-                </a>
-            </div>
+    <main class="relative">
+        {{--        <div class="absolute w-full h-full top-0 z-10"> --}}
+        {{--            <div class="  w-full h-full opacity-50 z-10" style=""> --}}
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+        {{--            </div> --}}
+        {{--            --}}{{--            <div class=" w-full opacity-90 h-1/2 z-10" style="background-color:#051937 "> --}}
+        {{--            --}}{{--            </div> --}}
+        {{--            --}}{{--            <div class="bg-blue-100 opacity-75  w-full h-1/2 z-10"> --}}
+        {{--            --}}{{--            </div> --}}
+        {{--        </div> --}}
+        @yield('content', $slot ?? '')
+    </main>
+
+    {{-- Footer --}}
+    <footer>
+        @include('layouts.footer')
+    </footer>
+
+    {{-- This directive is used to include the Livewire scripts --}}
+    @livewireScripts
+
+</body>
+
 </html>
