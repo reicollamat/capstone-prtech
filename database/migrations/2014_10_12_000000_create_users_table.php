2409e2560a0dce9b5418046da219ee672f24b517
richmondb <richmond.billones@gmail.com>
2023-11-13 23:02:40 +0800

    added columns in user, create seller info for future

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,16 +15,26 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
+            $table->string('profile_picture')->nullable();
+            $table->timestamp('birthdate')->nullable();
+            $table->string('sex')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_number')->nullable();
 
             // address
-            $table->string('street_address')->nullable();
+            $table->string('street_address_1')->nullable();
+            $table->string('street_address_2')->nullable();
+            $table->string('state_province')->nullable();
             $table->string('city')->nullable();
             $table->string('postal_code')->nullable();
             $table->string('country')->nullable();
+
+            // seller account specific
+            $table->boolean('is_seller')->default(false)->nullable();
+            $table->unsignedBigInteger('seller_id')->default(null)->nullable();
+
 
             $table->rememberToken();
             $table->timestamps();
