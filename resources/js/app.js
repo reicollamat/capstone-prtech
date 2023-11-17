14bca241a24ca9c606d3f85bba8d3df50d81864e
richmondb <richmond.billones@gmail.com>
2023-11-18 00:35:50 +0800

    some things

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,20 +1,43 @@
 import "./bootstrap";
 
+import {
+    Livewire,
+    Alpine,
+} from "../../vendor/livewire/livewire/dist/livewire.esm";
+import collapse from "@alpinejs/collapse";
+
+Alpine.plugin(collapse);
+
+Livewire.start();
+
 // Import our custom CSS
 import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
 
-// // Disable alpine plugin for now for livewire
-// import Alpine from 'alpinejs';
-// //
-// window.Alpine = Alpine;
-// //
-// Alpine.start();
+// this will remove the navbar height from the page height to remove scrolling
+function setChildContainerHeight() {
+    // get the height if main wrapper
+    let parentHeight = document.getElementById("main").clientHeight;
+    // get the height of navigation bar
+    let otherElementHeight =
+        document.getElementById("navigationbar").clientHeight;
+    // get the child container where we need to set the height
+    let childContainer = document.getElementById("wrapper");
+    // apply the height to the element
+    childContainer.style.height = parentHeight - otherElementHeight + "px";
+
+    console.log(parentHeight, otherElementHeight, childContainer.style.height);
+}
+
+// event listener to adjust the height of the child container
+window.addEventListener("load", setChildContainerHeight);
+window.addEventListener("resize", setChildContainerHeight);
 
 document.addEventListener("livewire:navigated", () => {
     console.log("navigated");
     // Runs immediately after Livewire has finished initializing
     // on the page...
+    setChildContainerHeight();
 });
