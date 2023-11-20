6c32a5742a570e8fb3c89ac4e4d96c572ac797d2
reicollamat <raycollamat11@gmail.com>
2023-11-21 03:01:11 +0800

    update purchase page

--- resources/views/pages/purchase.blade.php
+++ resources/views/pages/purchase.blade.php
@@ -13,166 +13,140 @@
 <!-- Checkout Start -->
 <div class="container-fluid">
     <div class="row px-xl-5">
-        <div class="col-lg-8 bg-dark">
+        <div class="col-lg-8">
             
             <div class="p-30 mb-5">
-                <h5 class="section-title position-relative text-uppercase mb-3 pb-3 text-light"><span class=" pr-3">Billing Address</span></h5>
+                <h5 class="section-title position-relative text-uppercase"><span class=" pr-3">Account Details</span></h5>
                 <div class="row">
                     <div class="col-md-6 form-group">
                         <label>First Name</label>
-                        <input class="form-control bg-primary text-light" type="text" value="{{$user->first_name}}" readonly>
+                        <input class="form-control bg-secondary text-light" type="text" value="{{$user->first_name}}" readonly>
                     </div>
                     <div class="col-md-6 form-group">
                         <label>Last Name</label>
-                        <input class="form-control bg-primary text-light" type="text" value="{{$user->last_name}}" readonly>
+                        <input class="form-control bg-secondary text-light" type="text" value="{{$user->last_name}}" readonly>
                     </div>
                     <div class="col-md-6 form-group">
                         <label>E-mail</label>
-                        <input class="form-control bg-primary text-light" type="text" value="{{$user->email}}" readonly>
+                        <input class="form-control bg-secondary text-light" type="text" value="{{$user->email}}" readonly>
                     </div>
                     <div class="col-md-6 form-group">
                         <label>Mobile No</label>
-                        <input class="form-control bg-primary text-light" type="text" value="{{$user->phone_number}}" readonly>
+                        <input class="form-control bg-secondary text-light" type="text" value="{{$user->phone_number}}" readonly>
+                    </div>
+                    <h5 class="section-title position-relative text-uppercase mt-4">
+                        Delivery Address
+                        <button type="button" class="btn text-primary p-0" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            Change
+                        </button>
+                    </h5>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Delivery Address</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col-md-6 form-group">
+                                    <label>City</label>
+                                    <input class="form-control bg-secondary text-light" type="text" value="{{$user->country}}" readonly>
+                                </div>
+                                <div class="col-md-6 form-group">
+                                    <label>Barangay</label>
+                                    <input class="form-control bg-secondary text-light" type="text" value="{{$user->city}}" readonly>
+                                </div>
+                                <div class="col-md-12 form-group">
+                                    <label>Street Address</label>
+                                    <input class="form-control bg-secondary text-light" type="text" value="{{$user->street_address}}" readonly>
+                                </div>
+                                <div class="col-md-6 form-group">
+                                    <label>Postal Code</label>
+                                    <input class="form-control bg-secondary text-light" type="text" value="{{$user->postal_code}}" readonly>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Change</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="col-md-6 form-group">
+                        <label>City</label>
+                        <input class="form-control bg-secondary text-light" type="text" value="{{$user->country}}" readonly>
+                    </div>
+                    <div class="col-md-6 form-group">
+                        <label>Barangay</label>
+                        <input class="form-control bg-secondary text-light" type="text" value="{{$user->city}}" readonly>
                     </div>
                     <div class="col-md-12 form-group">
                         <label>Street Address</label>
-                        <input class="form-control bg-primary text-light" type="text" value="{{$user->street_address}}" readonly>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label>Country</label>
-                        <input class="form-control bg-primary text-light" type="text" value="{{$user->country}}" readonly>
-                    </div>
-                    <div class="col-md-6 form-group">
-                        <label>City</label>
-                        <input class="form-control bg-primary text-light" type="text" value="{{$user->city}}" readonly>
+                        <input class="form-control bg-secondary text-light" type="text" value="{{$user->street_address}}" readonly>
                     </div>
                     <div class="col-md-6 form-group">
                         <label>Postal Code</label>
-                        <input class="form-control bg-primary text-light" type="text" value="{{$user->postal_code}}" readonly>
-                    </div>
-                    <div class="col-md-12 form-group">
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="confirm">
-                            <label class="custom-control-label" for="confirm">Confirm details</label>
-                        </div>
-                    </div>
-                    <div class="col-md-12">
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="shipto">
-                            <label class="custom-control-label" for="shipto" data-toggle="collapse" data-target="#shipping-address">Ship to different address</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="collapse mb-5" id="shipping-address">
-                
-                <div class="p-30">
-                    <h5 class="section-title position-relative text-uppercase mb-3 pb-3 text-light"><span class="pr-3">Shipping Address</span></h5>
-                    <div class="row">
-                        <div class="col-md-6 form-group">
-                            <label>First Name</label>
-                            <input class="form-control bg-primary text-light" type="text" value="{{$user->first_name}}">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Last Name</label>
-                            <input class="form-control bg-primary text-light" type="text" value="{{$user->last_name}}">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>E-mail</label>
-                            <input class="form-control bg-primary text-light" type="text" value="{{$user->email}}">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Mobile No</label>
-                            <input class="form-control bg-primary text-light" type="text" value="{{$user->phone_number}}">
-                        </div>
-                        <div class="col-md-12 form-group">
-                            <label>Street Address</label>
-                            <input class="form-control bg-primary text-light" type="text" value="{{$user->street_address}}">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Country</label>
-                            <input class="form-control bg-primary text-light" type="text" value="{{$user->country}}">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>City</label>
-                            <input class="form-control bg-primary text-light" type="text" value="{{$user->city}}">
-                        </div>
-                        <div class="col-md-6 form-group">
-                            <label>Postal Code</label>
-                            <input class="form-control bg-primary text-light" type="text" value="{{$user->postal_code}}">
-                        </div>
-                        <div class="col-md-12 form-group">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="confirm">
-                                <label class="custom-control-label" for="confirm">Confirm details</label>
-                            </div>
-                        </div>
+                        <input class="form-control bg-secondary text-light" type="text" value="{{$user->postal_code}}" readonly>
                     </div>
                 </div>
             </div>
         </div>
 
         <div class="col-lg-4">
-            
-            <div class="text-dark p-30 mb-5">
-                <h5 class="section-title position-relative text-uppercase mb-3 pb-3"><span class="pr-3">Purchase Total</span></h5>
-                <div class="border-bottom">
-                    <h6 class="mb-3">Products</h6>
-                    <div class="d-flex justify-content-between">
+            <div class="card mb-2">
+                <h4 class="card-header text-center">
+                    Purchase Summary
+                </h4>
+                <div class="card-body">
+                    <h5 class="mb-3">Products</h5>
+                    <div class="d-flex justify-content-between border-bottom">
                         <p>{{$quantity}}x {{$product->title}}</p>
-                        <p>= ₱ {{$product->price * $quantity}}</p>
+                        <p>₱{{$product->price * $quantity}}</p>
                     </div>
-                </div>
-                <div class="border-bottom pt-3 pb-2">
-                    <div class="d-flex justify-content-between mb-3">
-                        <h6>Subtotal</h6>
-                        <h6>₱{{$subtotal}}</h6>
+                    <div class="border-bottom pt-3 pb-2">
+                        <div class="d-flex justify-content-between mb-3">
+                            <h6>Subtotal</h6>
+                            <h6>₱{{$subtotal}}</h6>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <h6 class="font-weight-medium">Shipping</h6>
+                            <h6 class="font-weight-medium">₱ {{$shipping_value}}</h6>
+                        </div>
                     </div>
-                    <div class="d-flex justify-content-between">
-                        <h6 class="font-weight-medium">Shipping</h6>
-                        <h6 class="font-weight-medium">₱ {{$shipping_value}}</h6>
-                    </div>
-                </div>
-                <div class="pt-2 bg-primary">
-                    <div class="d-flex justify-content-between mt-2 mx-2">
-                        <h5>Total</h5>
-                        <h5>₱ {{$total}}</h5>
+                    <div class="pt-2 bg-primary-subtle">
+                        <div class="d-flex justify-content-between mt-2 mx-2">
+                            <h5>Total</h5>
+                            <h5>₱{{$total}}</h5>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="text-dark mb-5">
-                
-                <div class="p-30">
-                    <h5 class="section-title position-relative text-uppercase mb-3 pb-3"><span class="pr-3">Payment</span></h5>
-                    
+            <div class="card">
+                <h4 class="card-header text-center">
+                    Payment
+                </h4>
+                <div class="card-body mx-auto">
                     <form action="{{route('purchase_one')}}" method="POST">
                         @csrf
 
-                        <div class="form-group">
-                            <div class="custom-control custom-radio">
-                                <input type="radio" class="custom-control-input" name="payment_type" id="paypal" value="paypal" required>
-                                <label class="custom-control-label" for="paypal">Paypal</label>
-                            </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="radio" name="payment_type" id="cod" value="cod" required checked>
+                            <label class="form-check-label" for="cod">
+                                COD (Cash On Delivery)
+                            </label>
                         </div>
-                        <div class="form-group">
-                            <div class="custom-control custom-radio">
-                                <input type="radio" class="custom-control-input" name="payment_type" id="gcash" value="gcash" required>
-                                <label class="custom-control-label" for="gcash">GCash</label>
-                            </div>
+                        <div class="form-check mb-4">
+                            <input class="form-check-input" type="radio" name="payment_type" id="gcash" value="gcash" required>
+                            <label class="form-check-label" for="gcash">
+                                Gcash
+                            </label>
                         </div>
-                        <div class="form-group">
-                            <div class="custom-control custom-radio">
-                                <input type="radio" class="custom-control-input" name="payment_type" id="cod" value="cod" required>
-                                <label class="custom-control-label" for="cod">COD (Cash On Delivery)</label>
-                            </div>
-                        </div>
-                        <div class="form-group mb-4">
-                            <div class="custom-control custom-radio">
-                                <input type="radio" class="custom-control-input" name="payment_type" id="banktransfer" value="banktransfer" required>
-                                <label class="custom-control-label" for="banktransfer">Bank Transfer</label>
-                            </div>
-                        </div>
-
+                        
                         <input type="text" name="product_id" value="{{$product->id}}" hidden>
                         <input type="text" name="category" value="{{$product->category}}" hidden>
                         <input type="text" name="subtotal" value="{{$subtotal}}" hidden>
@@ -180,15 +154,25 @@
                         <input type="text" name="quantity" value="{{$quantity}}" hidden>
                         <input type="text" name="user_id" value="{{Auth::user()->id}}" hidden>
 
-                        <button class="btn btn-block btn-primary font-weight-bold py-3" type="submit">
-                            <div class="text-light">Confirm Purchase</div>
-                        </button>
+                        <div class="w-full mb-4">
+                            <button class="flex w-full no-underline decoration-0 text-black" type="submit">
+                                <span
+                                    class="lg:!h-12 w-full  h-10 flex items-center justify-center uppercase font-semibold px-4 lg:!px-6 border border-black hover:bg-blue-500 hover:text-white transition duration-500 ease-in-out">
+                                    Confirm Purchase
+                                </span>
+                            </button>
+                        </div>
                     </form>
                     <form action="{{route('index_cart')}}" method="GET">
                         @csrf
-                        <button type="submit" class="btn btn-dark btn-plus mt-4">
-                            Cancel
-                        </button>
+                        <div class="w-full">
+                            <button class="flex w-full no-underline decoration-0 text-light" type="submit">
+                                <span
+                                    class="lg:!h-12 w-full  h-10 flex items-center justify-center uppercase font-semibold px-4 lg:!px-6 border border-black bg-gray-800 hover:bg-white hover:text-black transition duration-500 ease-in-out">
+                                    Cancel
+                                </span>
+                            </button>
+                        </div>
                     </form>
                 </div>
             </div>
