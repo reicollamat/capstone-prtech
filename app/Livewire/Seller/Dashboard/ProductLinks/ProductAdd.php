83618d3e55dd7999941aaf85e0b7213d8a78c2ba
richmondb <richmond.billones@gmail.com>
2023-11-22 03:07:00 +0800

    new page for adding products with dynamic forms for each category

--- app/Livewire/Seller/Dashboard/ProductLinks/ProductAdd.php
+++ app/Livewire/Seller/Dashboard/ProductLinks/ProductAdd.php
@@ -8,6 +8,25 @@ use Livewire\Component;
 #[Layout('layouts.seller.seller-layout')]
 class ProductAdd extends Component
 {
+    public $view = 'component.category-component.placeholder-component';
+
+    public array $categoryViewMap = [
+
+    ];
+
+    public $productCategory;
+
+    public function changeCategoryView($category)
+    {
+        $this->productCategory = $category;
+        $this->view = '';
+    }
+
+    public function mount()
+    {
+        //        $this->view = 'test';
+    }
+
     public function render()
     {
         return view('livewire..seller.dashboard.product-links.product-add');
