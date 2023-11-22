3bc3c2918a301085d8d5bbbe806718efe0c002c9
richmondb <richmond.billones@gmail.com>
2023-11-22 23:13:33 +0800

    addded middleware for running ngrok

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\UpgradeToHttpsUnderNgrok;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -37,6 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \RalphJSmit\Livewire\Urls\Middleware\LivewireUrlsMiddleware::class,
+            UpgradeToHttpsUnderNgrok::class,
         ],
 
         'api' => [
