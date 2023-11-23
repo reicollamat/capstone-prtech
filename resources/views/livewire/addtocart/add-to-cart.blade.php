716b325ca2be58a3bba4937568d4335cafa6f418
reicollamat <raycollamat11@gmail.com>
2023-11-24 01:54:49 +0800

    fix duplicate

--- resources/views/livewire/addtocart/add-to-cart.blade.php
+++ resources/views/livewire/addtocart/add-to-cart.blade.php
@@ -1,4 +1,15 @@
 <div>
+    {{-- session flash notification --}}
+    {{-- for add-to-cart --}}
+    <div class="alert alert-primary rounded alert-dismissible fade show" role="alert" style="display: none; position: fixed; top: 20px; left: 25%; width: 50%; z-index:9999;" 
+        x-data="{show: false}" 
+        x-show="show"
+        x-transition:leave.duration.500ms
+        x-init="@this.on('notif-alert-cart', () => { show = true; setTimeout(() => { show = false;}, 5000) })">
+            {{ session('notification') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    
     <button wire:click="addtocart"
             class="text-white bg-gray-600 hover:bg-blue-800 focus:ring-4 no-underline focus:outline-none focus:ring-blue-300
             font-medium rounded-lg p-1.5 text-sm  items-center text-center">
