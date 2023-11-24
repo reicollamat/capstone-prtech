178e9a737c4b4f82ae1937f296c297255f38632b
reicollamat <raycollamat11@gmail.com>
2023-11-25 06:15:39 +0800

    updates from prtech-ui repo

--- resources/views/support/supportcenter.blade.php
+++ resources/views/support/supportcenter.blade.php
@@ -1,214 +1,102 @@
 @extends('layouts.master_layout')
 @section('content')
+    <style>
+        #content {
+        max-width: 1300px;
+        margin: 0 auto;
+        padding: 8px;
+        }
+        .card:hover {
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+        transition: box-shadow 0.3s ease-in-out;
+        }
+    </style>
     <div>
         <x-shop.breadcrumb>
             <span class="breadcrumb-item active">Support Center</span>
         </x-shop.breadcrumb>
     </div>
-    <div class="header d-flex justify-center  mt-3 mb-8">
+    <div class="header d-flex justify-content-center my-3">
         <h1>Support Center</h1>
     </div>
     <div class="container-fluid px-5">
         <div class="p-5">
-            <div class="justify-content-center" id="content">
-                <p class="mb-14 text-center">
-                    This website is operated by PR-TECH. Throughout this site, “we,”
-                    “us,” and “our” refer to PR-TECH. By using our site or purchasing
-                    from us, you agree to our Terms of Service. Please read these
-                    carefully. If you disagree, you may not access the website. Your use
-                    of our site is conditioned on your acceptance of all terms and
-                    conditions, which apply to all users, including browsers, vendors,
-                    customers, and contributors. New features or tools added to the site
-                    are subject to these terms. We reserve the right to update, change,
-                    or replace any part of the Terms of Service by posting updates on
-                    the website. Your continued use of the site means you accept any
-                    modifications.
+        <div class="justify-content-center" id="content">
+            <p class="mb-14 text-center">
+            Have questions? You're in the right place! Below is our Frequently
+            Asked Questions (FAQ) section. This is designed to provide you with
+            quick and comprehensive answers to the most common queries. Whether
+            you're a new visitor or a seasoned user, this resource is here to
+            guide you through various aspects of our platform, products, or
+            services.
+            </p>
+
+            <!-- Delivery Info Card -->
+            <div class="container mt-5">
+            <div class="card p-4">
+                <h5>ORDERING AND DELIVERY INFORMATION</h5>
+                <p>
+                <strong>How to Order:</strong>
                 </p>
-                <!-- Cards -->
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card p-4">
-                                <h5 class="card-title">SECTION 1 - ONLINE STORE TERMS</h5>
-                                <p>
-                                    You confirm you're of legal age to use our site, and you
-                                    won't use our products for illegal purposes or violate any
-                                    laws. Transmitting harmful code is prohibited, and a
-                                    violation leads to immediate termination of services.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 p-4">
-                                <h5 class="card-title">SECTION 2 - GENERAL CONDITIONS</h5>
-                                <p>
-                                    We can refuse service for any reason, and your content may
-                                    be transferred and changed for technical reasons.
-                                    Reproducing or exploiting our services without permission is
-                                    not allowed. Headings are for convenience and don't limit
-                                    the terms.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 p-4">
-                                <h5 class="card-title">
-                                    SECTION 3 - ACCURACY, COMPLETENESS, AND TIMELINESS OF
-                                    INFORMATION
-                                </h5>
-                                <p>
-                                    We're not responsible if information is inaccurate,
-                                    incomplete, or outdated. Historical info may not be current,
-                                    and we may modify site contents; it's your responsibility to
-                                    stay updated.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 p-4">
-                                <h5 class="card-title">
-                                    SECTION 4 - MODIFICATIONS TO THE SERVICE AND PRICES
-                                </h5>
-                                <p>
-                                    Product prices and services are subject to change without
-                                    notice. We can modify or discontinue the service at any
-                                    time, and we're not liable for changes, suspensions, or
-                                    discontinuance of the service.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 p-4">
-                                <h5 class="card-title">SECTION 5 - PRODUCTS OR SERVICES</h5>
-                                <p>
-                                    Some products or services are exclusively available online.
-                                    They have limited quantities and are subject to
-                                    return/exchange per our Return Policy. We strive for
-                                    accurate product displays, but we can't guarantee monitor
-                                    color accuracy.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 p-4">
-                                <h5 class="card-title">
-                                    SECTION 6 - ACCURACY OF BILLING AND ACCOUNT INFORMATION
-                                </h5>
-                                <p>
-                                    We reserve the right to refuse any order and may limit or
-                                    cancel quantities. Providing accurate purchase and account
-                                    information is essential.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 p-4">
-                                <h5 class="card-title">SECTION 7 - OPTIONAL TOOLS</h5>
-                                <p>
-                                    Third-party tools are provided as-is; use them at your own
-                                    risk. We may provide you with access to third-party tools
-                                    over which we neither monitor nor have any control nor
-                                    input. You acknowledge and agree that we provide access to
-                                    such tools "as is" and "as available" without any
-                                    warranties, representations, or conditions of any kind and
-                                    without any endorsement. We shall have no liability
-                                    whatsoever arising from or relating to your use of optional
-                                    third-party tools. Any use by you of optional tools offered
-                                    through the site is entirely at your own risk and
-                                    discretion, and you should ensure that you are familiar with
-                                    and approve of the terms on which tools are provided by the
-                                    relevant third-party provider(s).
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 p-4">
-                                <h5 class="card-title">SECTION 8 - THIRD-PARTY LINKS</h5>
-                                <p>
-                                    Content from third parties isn't our responsibility. Review
-                                    third-party policies before engaging in transactions. Some
-                                    content, products, and services available via our Service
-                                    may include materials from third-parties. Third-party links
-                                    on this site may direct you to third-party websites that are
-                                    not affiliated with us. We are not responsible for examining
-                                    or evaluating the content or accuracy, and we do not warrant
-                                    and will not have any liability or responsibility for any
-                                    third-party materials or websites, or for any other
-                                    materials, products, or services of third-parties. We are
-                                    not liable for any harm or damages related to the purchase
-                                    or use of goods, services, resources, content, or any other
-                                    transactions made in connection with any third-party
-                                    websites. Please review carefully the third-party's policies
-                                    and practices and make sure you understand them before you
-                                    engage in any transaction. Complaints, claims, concerns, or
-                                    questions regarding third-party products should be directed
-                                    to the third-party.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 p-4">
-                                <h5 class="card-title">
-                                    SECTION 9 - USER COMMENTS, PERSONAL INFORMATION, ERRORS,
-                                    PROHIBITED USES, DISCLAIMER, INDEMNIFICATION, SEVERABILITY
-                                </h5>
-                                <p>
-                                    User comments are subject to use by us. Personal information
-                                    is governed by our Privacy Policy. We're not liable for
-                                    errors, inaccuracies, or omissions. Prohibited uses include
-                                    unlawful activities and interference with security features.
-                                    We're not liable for service interruptions or termination.
-                                    Indemnify us against claims arising from your breach. If a
-                                    provision is unlawful, the rest remains valid.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card mt-3 mb-10 p-4">
-                                <h5 class="card-title">
-                                    SECTION 10 - TERMINATION, ENTIRE AGREEMENT, GOVERNING LAW,
-                                    CHANGES TO TERMS, CONTACT INFORMATION
-                                </h5>
-                                <p>
-                                    Termination doesn't affect prior obligations. The Terms of
-                                    Service govern your use and supersede prior agreements.
-                                    Governing law is in accordance with the laws of the
-                                    Philippines. We can change these terms, so check
-                                    periodically. Contact us at support@prtech.com.ph with any
-                                    questions.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <ul>
+                <li style="margin-bottom: 10px">
+                    &#x2022; Add item to your cart.
+                </li>
+                <li style="margin-bottom: 10px">
+                    &#x2022; Click on the Cart icon in the upper right corner to
+                    view your cart.
+                </li>
+                <li style="margin-bottom: 10px">
+                    &#x2022; Click on the Checkout button.
+                </li>
+                <li style="margin-bottom: 10px">
+                    &#x2022; Enter Billing and Shipping Details.
+                </li>
+                <li style="margin-bottom: 10px">
+                    &#x2022; Select a payment option.
+                </li>
+                <li style="margin-bottom: 10px">
+                    &#x2022; Click on Confirm Order Button.
+                </li>
+                </ul>
+                <p>
+                <strong>Payment Options:</strong>
+                <br />Cash on Delivery (COD) based on your desired courier or
+                GCash.
+                </p>
+                <p>
+                <strong>How do I receive my order?</strong>
+                <br />Orders can be received via delivery.
+                </p>
+                <p>
+                <strong>Shipping:</strong>
+                <br />Shipping is currently available nationwide thru LBC, JRS,
+                J&amp;T, or Lalamove.
+                </p>
+                <p>
+                <strong>How to Track My Order:</strong>
+                <br />When logged in, go to My Account to access your dashboard.
+                You may see the status of your orders on the order history.
+                </p>
             </div>
+
+            <!-- Refund Information Card -->
+            <div class="card mt-3 p-4">
+                <h5>Refund Information</h5>
+                <p>
+                To initiate a refund, email sales@prtech.com.ph with the subject
+                "Refund Request" and include your order number, refund amount,
+                and concerns in the body. An Online Sales Representative will
+                contact you within 24 hours. For item returns, contact
+                support@prtech.com.ph or send a message in our
+                <a href="http://localhost:3333/contactus.html">contact us</a>
+                page for instructions before proceeding with the return process.
+                Please refer to the warranty information for comprehensive
+                details on our Item Warranty Policy.
+                </p>
+            </div>
+            </div>
+        </div>
         </div>
     </div>
 @endsection
