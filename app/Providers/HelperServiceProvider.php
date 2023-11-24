0adf7650badc72f1480934696df138f1d2fd1bb5
richmondb <richmond.billones@gmail.com>
2023-11-24 14:34:50 +0800

    fix tings

--- app/Providers/HelperServiceProvider.php
+++ app/Providers/HelperServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class HelperServiceProvider extends ServiceProvider
@@ -11,10 +12,12 @@ class HelperServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // $files = glob(app_path('Helper').'/*.php');
-        // foreach ($files as $key => $file) {
-        //     require_once $file;
-        // }
+        $files = glob(app_path('Helpers').'/*.php');
+        foreach ($files as $key => $file) {
+            require_once $file;
+            Log::channel('stderr')->info("Loaded $file\n");
+        }
+
     }
 
     /**
