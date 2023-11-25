7a11f0c68209900f2a259e4a43a62ab221725972
richmondb <richmond.billones@gmail.com>
2023-11-26 01:13:42 +0800

    added chart js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import path from 'path'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import path from "path";
+
 export default defineConfig({
     // root: path.resolve(__dirname, 'src'),
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/charts/analytics-model-report-header.js",
             ],
             refresh: true,
         }),
-
     ],
     resolve: {
         alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
+            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+        },
     },
 });
