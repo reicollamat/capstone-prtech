9a9d0ccf751a9203ea9c68a497262383ede6ca25
richmondb <richmond.billones@gmail.com>
2023-11-26 01:13:27 +0800

    nag bubug ata ito so comment muna

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \RalphJSmit\Livewire\Urls\Middleware\LivewireUrlsMiddleware::class,
-            UpgradeToHttpsUnderNgrok::class,
+            // UpgradeToHttpsUnderNgrok::class,
         ],
 
         'api' => [
