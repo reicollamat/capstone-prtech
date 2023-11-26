147b8fc75da99eb30714784f9e95e8ae411c3cf4
richmondb <richmond.billones@gmail.com>
2023-11-26 18:06:41 +0800

    added ui for order inforamtion sent to the email of user testing only

--- app/Livewire/Landing.php
+++ app/Livewire/Landing.php
@@ -2,8 +2,9 @@
 
 namespace App\Livewire;
 
-use App\Models\Product;
-use Livewire\Attributes\Computed;
+use App\Mail\OrderShipped;
+use Exception;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -28,13 +29,6 @@ class Landing extends Component
         HTML;
     }
 
-    #[Computed]
-    public function getProducts()
-    {
-        return Product::paginate(10);
-
-    }
-
     public function render()
     {
         return view('livewire.landing');
@@ -44,5 +38,19 @@ class Landing extends Component
     public function increment()
     {
         $this->count++;
+    }
+
+    public string $mailStatus;
+
+    public function sendMail()
+    {
+        // hinde ko alam kung bakit pero pag unang pindot dito nag rereload ung page, pero minsan inde naman
+        // TODO: pass the id of order inside OrderShipped function, then query the order based on that id on the OrderShipped.php to display the items and total??
+        try {
+            Mail::to('richmond.billones@gmail.com')->send(new OrderShipped());
+            $this->mailStatus = 'Email Sent';
+        } catch (Exception $e) {
+            $this->mailStatus = 'There was an error sending the email: '.$e->getMessage();
+        }
     }
 }
---
+++ app/Mail/OrderShipped.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OrderShipped extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address('prtech.business@gmail.com', 'PR-TECH'),
+            subject: 'PR-TECH Order Process Complete',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.ordershipped-template',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
