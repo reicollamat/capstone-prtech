75dbe9545bb543a028a901492417e8bc13a85f5d
richmondb <richmond.billones@gmail.com>
2023-11-26 22:23:48 +0800

    tings

--- resources/views/livewire/shop/collections.blade.php
+++ resources/views/livewire/shop/collections.blade.php
@@ -3,20 +3,19 @@
     {{-- session flash notification --}}
     <x-notification-alert>
         @if (session('notification-livewire'))
-        {{ session('notification-livewire') }}
+            {{ session('notification-livewire') }}
         @else
-        {{ session('notification') }}
+            {{ session('notification') }}
         @endif
     </x-notification-alert>
 
     {{-- session flash notification --}}
     {{-- for add-to-wishlist --}}
-    <div class="alert alert-primary rounded alert-dismissible fade show" role="alert" style="display: none; position: fixed; top: 20px; left: 25%; width: 50%; z-index:9999;" 
-        x-data="{show: false}" 
-        x-show="show"
-        x-transition:leave.duration.500ms
-        x-init="@this.on('notif-alert-wishlist', () => { show = true; setTimeout(() => { show = false;}, 5000) })">
-            {{ session('notification-livewire') }}
+    <div class="alert alert-primary rounded alert-dismissible fade show" role="alert"
+        style="display: none; position: fixed; top: 20px; left: 25%; width: 50%; z-index:9999;" x-data="{ show: false }"
+        x-show="show" x-transition:leave.duration.500ms x-init="@this.on('notif-alert-wishlist', () => { show = true;
+            setTimeout(() => { show = false; }, 5000) })">
+        {{ session('notification-livewire') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 
@@ -661,7 +660,7 @@
                             @endforeach
                             {{--                            {{ $this->getProducts->links() }} --}}
                         </div>
-                        <div class="p-3">
+                        <div class="p-3 flex justify-center">
                             {{ $this->getProducts->links() }}
                         </div>
 
