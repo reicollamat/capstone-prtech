c58ba94613a1a4d1d3a2b59d143b4063164c80ff
richmondb <richmond.billones@gmail.com>
2023-11-27 15:36:24 +0800

    example of wrapping them in form. line start 6 and end 189

--- resources/views/livewire/component/category-component/cpu-component.blade.php
+++ resources/views/livewire/component/category-component/cpu-component.blade.php
@@ -3,116 +3,119 @@
     {{ $productName }}
     {{ $productSKU }}
     <div class="grid md:grid-cols-2 gap-4">
-        <div>
-            <!-- Brand and Price -->
-            <div class="grid md:grid-cols-2 gap-4">
+        <form wire:submit.prevent="submit">
+            <div>
+                <!-- Brand and Price -->
+                <div class="grid md:grid-cols-2 gap-4">
+                    <div class="mb-4">
+                        <label for="cpu_name"
+                            class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">CPU
+                            Brand, Name, Title
+                        </label>
+                        <input type="text" id="cpu_name" wire:model.blur="cpu_name"
+                            class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="AMD, Intel, etc." required>
+                        @error('cpu_name')
+                            <span class="font-sm text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="price"
+                            class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">Price</label>
+                        <input type="text" id="price" wire:model.blur="price"
+                            class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="In Pesos" required>
+                        @error('price')
+                            <span class="font-sm text-red-500">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+
+                <!-- CPU Base Clock -->
                 <div class="mb-4">
-                    <label for="cpu_name" class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">CPU
-                        Brand, Name, Title
-                    </label>
-                    <input type="text" id="cpu_name" wire:model.blur="cpu_name"
+                    <label for="base_clock"
+                        class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">Base
+                        Clock</label>
+                    <input type="text" id="base_clock" wire:model.blur="base_clock"
                         class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="AMD, Intel, etc." required>
-                    @error('cpu_name')
+                        placeholder="2.9 Ghz" required>
+                    @error('base_clock')
                         <span class="font-sm text-red-500">{{ $message }}</span>
                     @enderror
                 </div>
 
+                <!-- CPU Boost Clock -->
                 <div class="mb-4">
-                    <label for="price"
-                        class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">Price</label>
-                    <input type="text" id="price" wire:model.blur="price"
+                    <label for="boost_clock"
+                        class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">Boost
+                        Clock</label>
+                    <input type="text" id="boost_clock" wire:model.blur="boost_clock"
                         class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="In Pesos" required>
-                    @error('price')
+                        placeholder="3.6 Ghz" required>
+                    @error('boost_clock')
+                        <span class="font-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- TDP -->
+                <div class="mb-4">
+                    <label for="tdp"
+                        class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">TDP</label>
+                    <input type="text" id="tdp" wire:model.blur="tdp"
+                        class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="65 Watts" required>
+                    @error('tdp')
+                        <span class="font-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- iGPU -->
+                <div class="mb-4">
+                    <label for="igpu"
+                        class="block mb-1 text-sm font-medium text-gray-800 dark:text-white pl-1">Integrated
+                        Graphics</label>
+                    <select id="igpu" wire:model.blur="igpu"
+                        class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required>
+                        <option disabled selected>Select Below</option>
+                        <option value="Yes">Yes</option>
+                        <option value="No">No</option>
+                    </select>
+                    @error('igpu')
+                        <span class="font-sm text-red-500">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <!-- UOC -->
+                <div class="mb-4">
+                    <label for="oc_unlocked"
+                        class="block mb-1 text-sm font-medium text-gray-800 dark:text-white pl-1">Unlocked
+                        for
+                        Overclocking</label>
+                    <select id="oc_unlocked" wire:model.blur="oc_unlocked"
+                        class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required>
+                        <option disabled selected>Select Below</option>
+                        <option value="Yes">Yes</option>
+                        <option value="No">No</option>
+                        <option value="Factory Overclocked">Factory Overclocked</option>
+                    </select>
+                    @error('oc_unlocked')
                         <span class="font-sm text-red-500">{{ $message }}</span>
                     @enderror
                 </div>
             </div>
 
-            <!-- CPU Base Clock -->
-            <div class="mb-4">
-                <label for="base_clock" class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">Base
-                    Clock</label>
-                <input type="text" id="base_clock" wire:model.blur="base_clock"
-                    class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="2.9 Ghz" required>
-                @error('base_clock')
-                    <span class="font-sm text-red-500">{{ $message }}</span>
-                @enderror
-            </div>
+            <div>
+                <!-- Add Product Image Div -->
+                <div class="pb-3">
+                    <p class="block mb-1 text-base font-medium text-gray-600 dark:text-white pl-1">Add Product Image</p>
+                    <p class="block mb-1 text-sm font-medium text-gray-500 dark:text-white pl-1">To Upload Multiple
+                        Images,
+                        Select them all before uploading</p>
+                </div>
 
-            <!-- CPU Boost Clock -->
-            <div class="mb-4">
-                <label for="boost_clock"
-                    class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">Boost
-                    Clock</label>
-                <input type="text" id="boost_clock" wire:model.blur="boost_clock"
-                    class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="3.6 Ghz" required>
-                @error('boost_clock')
-                    <span class="font-sm text-red-500">{{ $message }}</span>
-                @enderror
-            </div>
-
-            <!-- TDP -->
-            <div class="mb-4">
-                <label for="tdp"
-                    class="block mb-1 text-sm font-medium text-gray-800 dark:text-white  pl-1">TDP</label>
-                <input type="text" id="tdp" wire:model.blur="tdp"
-                    class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="65 Watts" required>
-                @error('tdp')
-                    <span class="font-sm text-red-500">{{ $message }}</span>
-                @enderror
-            </div>
-
-            <!-- iGPU -->
-            <div class="mb-4">
-                <label for="igpu"
-                    class="block mb-1 text-sm font-medium text-gray-800 dark:text-white pl-1">Integrated
-                    Graphics</label>
-                <select id="igpu" wire:model.blur="igpu"
-                    class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required>
-                    <option disabled selected>Select Below</option>
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
-                </select>
-                @error('igpu')
-                    <span class="font-sm text-red-500">{{ $message }}</span>
-                @enderror
-            </div>
-
-            <!-- UOC -->
-            <div class="mb-4">
-                <label for="oc_unlocked"
-                    class="block mb-1 text-sm font-medium text-gray-800 dark:text-white pl-1">Unlocked
-                    for
-                    Overclocking</label>
-                <select id="oc_unlocked" wire:model.blur="oc_unlocked"
-                    class="bg-white border border-gray-300 text-gray-900 text-sm !rounded focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    required>
-                    <option disabled selected>Select Below</option>
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
-                    <option value="Factory Overclocked">Factory Overclocked</option>
-                </select>
-                @error('oc_unlocked')
-                    <span class="font-sm text-red-500">{{ $message }}</span>
-                @enderror
-            </div>
-        </div>
-
-        <div>
-            <!-- Add Product Image Div -->
-            <div class="pb-3">
-                <p class="block mb-1 text-base font-medium text-gray-600 dark:text-white pl-1">Add Product Image</p>
-                <p class="block mb-1 text-sm font-medium text-gray-500 dark:text-white pl-1">To Upload Multiple Images,
-                    Select them all before uploading</p>
-            </div>
-
-            <form wire:submit.prevent="submit">
                 <div class="flex items-center justify-center w-full">
                     <label for="dropzone-file"
                         class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
@@ -139,48 +142,50 @@
                 <button type="submit">
                     Submit
                 </button>
-            </form>
 
-            <div wire:loading wire:target="productImages">Uploading...</div>
+                <div wire:loading wire:target="productImages">Uploading...</div>
 
-            <div class="py-3">
-                <p class="block mb-1 text-sm font-medium text-gray-600 dark:text-white  pl-1">Image Preview (Click Image
-                    To Preview)</p>
-            </div>
+                <div class="py-3">
+                    <p class="block mb-1 text-sm font-medium text-gray-600 dark:text-white  pl-1">Image Preview (Click
+                        Image
+                        To Preview)</p>
+                </div>
 
-            <div class="grid md:grid-cols-3 gap-1 h-auto">
-                @if ($productImages)
-                    @foreach ($productImages as $image)
-                        <!-- Button trigger modal -->
-                        <button type="button" class="" data-bs-toggle="modal" data-bs-target="#exampleModal"
-                            wire:click="$set('previewImage', '{{ $image->temporaryUrl() }}')">
-                            <img class="h-auto max-w-full border border-gray-400" src="{{ $image->temporaryUrl() }}"
-                                alt="image description">
-                        </button>
-                    @endforeach
-                @endif
-            </div>
+                <div class="grid md:grid-cols-3 gap-1 h-auto">
+                    @if ($productImages)
+                        @foreach ($productImages as $image)
+                            <!-- Button trigger modal -->
+                            <button type="button" class="" data-bs-toggle="modal" data-bs-target="#exampleModal"
+                                wire:click="$set('previewImage', '{{ $image->temporaryUrl() }}')">
+                                <img class="h-auto max-w-full border border-gray-400"
+                                    src="{{ $image->temporaryUrl() }}" alt="image description">
+                            </button>
+                        @endforeach
+                    @endif
+                </div>
 
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog modal-lg modal-dialog-centered">
-                    <div class="modal-content p-4">
-                        <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel">Image Preview</h1>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <img class="h-auto max-w-full border border-gray-400" src="{{ $previewImage }}"
-                                alt="Image Preview">
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog modal-lg modal-dialog-centered">
+                        <div class="modal-content p-4">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Image Preview</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <img class="h-auto max-w-full border border-gray-400" src="{{ $previewImage }}"
+                                    alt="Image Preview">
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
 </div>
