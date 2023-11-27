1d8664ea054841272a4c84dfa5c2bf992415aa49
richmondb <richmond.billones@gmail.com>
2023-11-27 15:00:09 +0800

    added some variables from handling the first part of add product

--- app/Livewire/Component/CategoryComponent/MemoryComponent.php
+++ app/Livewire/Component/CategoryComponent/MemoryComponent.php
@@ -2,10 +2,41 @@
 
 namespace App\Livewire\Component\CategoryComponent;
 
+use Livewire\Attributes\Reactive;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class MemoryComponent extends Component
 {
+    use WithFileUploads;
+
+    public $previewImage;
+
+    #[Reactive]
+    public $productName;
+
+    #[Reactive]
+    public $productSKU;
+
+    #[Reactive]
+    public $productSlug;
+
+    #[Reactive]
+    public $productDescription;
+
+    #[Reactive]
+    public $productCondition;
+
+    #[Reactive]
+    public $productStatus;
+
+    #[Reactive]
+    public $productCategory;
+
+    #[Validate(['productImages.*' => 'image|max:5120'])]
+    public $productImages = [];
+
     public function render()
     {
         return view('livewire.component.category-component.memory-component');
