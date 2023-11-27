9d5f5b89d779d46a43ca576d4329f3926d3606a2
reicollamat <raycollamat11@gmail.com>
2023-11-28 04:19:03 +0800

    add notification model (migrate)

---
+++ app/Models/Notification.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+
+    // relationship to User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'notif_category',
+        'title',
+        'message',
+    ];
+}
