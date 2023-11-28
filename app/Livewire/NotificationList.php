586e5e54b000b7da9e54af9edfa2ad4a6181585f
reicollamat <raycollamat11@gmail.com>
2023-11-28 17:54:47 +0800

    notification component

---
+++ app/Livewire/NotificationList.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\UserNotification;
+use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
+
+class NotificationList extends Component
+{
+    public $user_id;
+    public $notifications = [];
+    public $notifications_count = 0;
+
+    public function mount()
+    {
+
+        if (Auth::check()) {
+            $this->user_id = Auth::id();
+
+            // if (count(UserNotification::where('user_id', $this->user_id)->get())) {
+            //     dd(UserNotification::where('user_id', $this->user_id)->get());
+            // }
+            // dd(UserNotification::where('user_id', $this->user_id)->get());
+            $this->notifications = UserNotification::where('user_id', $this->user_id)->get();
+
+            $this->notifications_count = count($this->notifications);
+
+            // dd($this->notifications);
+        }
+    }
+
+    public function render()
+    {
+        return view('livewire.notification-list');
+    }
+}
