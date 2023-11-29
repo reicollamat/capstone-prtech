4d3b2e403d7c4c34067a43a1887b2f594247e33f
richmondb <richmond.billones@gmail.com>
2023-11-30 01:29:40 +0800

    remove debug

--- resources/views/livewire/shop/collections.blade.php
+++ resources/views/livewire/shop/collections.blade.php
@@ -22,11 +22,11 @@
     </div>
 
     <span>
-        {{ $sortingby }}
-        {{ $sortdirection }}
-        {{ $sortname }}
-        {{ $search }}
-        {{ $category }}
+        {{--        {{ $sortingby }} --}}
+        {{--        {{ $sortdirection }} --}}
+        {{--        {{ $sortname }} --}}
+        {{--        {{ $search }} --}}
+        {{--        {{ $category }} --}}
     </span>
 
     <div>
@@ -625,20 +625,16 @@
                                                     {{ $value->title }}</h5>
                                             </a>
                                             <div class="flex items-center mt-2.5 mb-2.5">
-                                                <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                    viewBox="0 0 22 20">
-                                                    <path
-                                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                                </svg>
-                                                <svg class="w-4 h-4 text-gray-200 dark:text-gray-600"
-                                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                                    fill="currentColor" viewBox="0 0 22 20">
-                                                    <path
-                                                        d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                                                </svg>
+                                                @for ($i = 0; $i < intval($value->rating); $i++)
+                                                    <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true"
+                                                        xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                        viewBox="0 0 22 20">
+                                                        <path
+                                                            d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                                                    </svg>
+                                                @endfor
                                                 <span
-                                                    class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">5.0</span>
+                                                    class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">{{ $value->rating }}</span>
                                             </div>
                                             <div class="mb-2.5">
                                                 <span class="text-xl font-medium text-gray-900 ">PHP
