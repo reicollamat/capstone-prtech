c807b02f64c3fe2a4c61ab157b8febbcdaf14bd2
richmondb <richmond.billones@gmail.com>
2023-12-06 23:26:31 +0800

    clean some ui

--- resources/views/email/ordershipped-template_2.blade.php
+++ resources/views/email/ordershipped-template_2.blade.php
@@ -3,6 +3,7 @@
     xmlns="http://www.w3.org/1999/xhtml"
     xmlns:v="urn:schemas-microsoft-com:vml"
     xmlns:o="urn:schemas-microsoft-com:office:office"
+    lang="en"
 >
 <head>
     <!--[if gte mso 9]>
