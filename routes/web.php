e022edee1e375f0a09b0c56724c263fed5589533
richmondb <richmond.billones@gmail.com>
2023-12-06 23:26:09 +0800

    added new route for metrics

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ use App\Livewire\Seller\Dashboard\ShipmentLinks\ShipmentList;
 use App\Livewire\Seller\Dashboard\ShipmentLinks\ShipmentOptions;
 use App\Livewire\Seller\Dashboard\ShopLinks\ShopManagement;
 use App\Livewire\Seller\Dashboard\ShopLinks\ShopManagementCategory;
+use App\Livewire\Seller\Dashboard\ShopLinks\ShopMetricSettings;
 use App\Livewire\Seller\OnBoarding\Form\SellerRegistration;
 use App\Livewire\Seller\OnBoarding\Form\ShopInformation;
 use App\Livewire\Shop;
@@ -109,6 +110,7 @@ Route::prefix('seller')->group(function () {
             Route::prefix('shop')->group(function () {
                 Route::get('/', Shopmanagement::class)->name('shop-management');
                 Route::get('/manage/category', ShopManagementCategory::class)->name('shop-management-category');
+                Route::get('/manage/metrics', ShopMetricSettings::class)->name('shop-management-metrics');
             });
             Route::prefix('analytics')->group(function () {
                 Route::get('/stocksense', AnalyticsModelReport::class)->name('analytics-model-report');
