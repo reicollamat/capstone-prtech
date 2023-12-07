bbba357c37f523689227c2938f43d05095e01276
richmondb <richmond.billones@gmail.com>
2023-12-07 23:21:58 +0800

    testing email

--- app/Livewire/Landing.php
+++ app/Livewire/Landing.php
@@ -2,14 +2,17 @@
 
 namespace App\Livewire;
 
+use App\Helpers\EmailHelper;
+use App\Helpers\ReferenceGeneratorHelper;
 use App\Mail\OrderShipped;
+use App\Models\Purchase;
+use App\Models\PurchaseItem;
 use Exception;
 use Illuminate\Support\Facades\Mail;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
-use Usernotnull\Toast\Concerns\WireToast;
 
 class Landing extends Component
 {
@@ -17,9 +20,13 @@ class Landing extends Component
     use WithPagination;
 
     public int $count = 0;
+    public string $mailStatus;
 
     public function mount()
     {
+        $this->purchase = Purchase::find(2);
+
+        // dd($this->purchase_items);
     }
 
     public function placeholder()
@@ -42,26 +49,52 @@ class Landing extends Component
         $this->count++;
     }
 
-    public string $mailStatus;
+    public $purchase;
 
     public function sendMail()
     {
-        // hinde ko alam kung bakit pero pag unang pindot dito nag rereload ung page, pero minsan inde naman
-        // TODO: pass the id of order inside OrderShipped function, then query the order based on that id on the OrderShipped.php to display the items and total??
-        try {
-            Mail::to('richmond.billones@gmail.com')->send(new OrderShipped());
-            $this->mailStatus = 'Email Sent';
-        } catch (Exception $e) {
-            $this->mailStatus = 'There was an error sending the email: '.$e->getMessage();
-        }
+        // // hinde ko alam kung bakit pero pag unang pindot dito nag rereload ung page, pero minsan inde naman
+        // // TODO: pass the id of order inside OrderShipped function, then query the order based on that id on the OrderShipped.php to display the items and total??
+        // try {
+        //     Mail::to('richmond.billones@gmail.com')->send(new OrderShipped());
+        //     $this->mailStatus = 'Email Sent';
+        // } catch (Exception $e) {
+        //     $this->mailStatus = 'There was an error sending the email: '.$e->getMessage();
+        // }
+
+        $this->mailStatus = EmailHelper::sendEmail('richmond.billones@gmail.com', Purchase::find(2));
+
+        $orders = Purchase::find(2);
+
+        // $purchase_items_product_information = P::find($orders->id);
+
+        // $this->purchase_items = PurchaseItem::all();
+
+        // dd($this->purchase_items);
+
+        // dd($orders->purchase_items);
+        // dd($purchase_items->product);
+        // dd($purchase_items_product_information);
+
+
+        //
+        // $reference = ReferenceGeneratorHelper::generateReferenceString();
+        //
+        // dd($reference);
+
+
+        // generate 16 unique reference string that consist of numberes and letters
+
+
     }
+
 
     public function tryAlert()
     {
         $this->alert('question', 'How are you today?', [
             'showConfirmButton' => true,
             'confirmButtonText' => 'Good',
-            'onConfirmed' => 'confirmed'
+            'onConfirmed' => 'confirmed',
         ]);
         // $this->alert('success', 'Successssssssssssssssssssssss', [
         //     'position' => 'top-end']);
