0342ef2b3f12d2184b2f01d598c99e8cffe33e54
richmondb <richmond.billones@gmail.com>
2023-12-11 03:00:25 +0800

    added shop metrics info

--- app/Livewire/Seller/Dashboard/ShopLinks/ShopMetricSettings.php
+++ app/Livewire/Seller/Dashboard/ShopLinks/ShopMetricSettings.php
@@ -2,13 +2,38 @@
 
 namespace App\Livewire\Seller\Dashboard\ShopLinks;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+
 #[Layout('layouts.seller.seller-layout')]
 class ShopMetricSettings extends Component
 {
+    #[Validate('required')]
+    public $targetSales;
+
+    public $currentTargetSales;
+
+    public function mount()
+    {
+        $shopMetrics = User::findOrFail(Auth::user()->id)->seller->shopMetrics->first() ?? 0;
+
+        // dd($shopMetrics->target_sales);
+        $this->currentTargetSales = $shopMetrics->target_sales ?? 0;
+    }
+
     public function render()
     {
         return view('livewire.seller.dashboard.shop-links.shop-metric-settings');
+    }
+
+    public function submit()
+    {
+        $validator = $this->validate([
+            'targetSales' => 'required',
+        ]);
+        dd($validator);
     }
 }
