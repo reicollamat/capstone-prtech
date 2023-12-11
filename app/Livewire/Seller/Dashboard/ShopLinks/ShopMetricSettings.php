2461c0880dbd6a96f1f9af4531d9cfc5bc1a9a5d
richmondb <richmond.billones@gmail.com>
2023-12-11 16:01:05 +0800

    added option to change the baseline value of metrics

--- app/Livewire/Seller/Dashboard/ShopLinks/ShopMetricSettings.php
+++ app/Livewire/Seller/Dashboard/ShopLinks/ShopMetricSettings.php
@@ -2,23 +2,35 @@
 
 namespace App\Livewire\Seller\Dashboard\ShopLinks;
 
+use App\Models\SellerShopMetrics;
 use App\Models\User;
+use Exception;
 use Illuminate\Support\Facades\Auth;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Locked;
 use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 #[Layout('layouts.seller.seller-layout')]
 class ShopMetricSettings extends Component
 {
+    use LivewireAlert;
+
     #[Validate('required')]
     public $targetSales;
 
+    #[Locked]
+    public $shopMetricsId;
+
     public $currentTargetSales;
+
 
     public function mount()
     {
         $shopMetrics = User::findOrFail(Auth::user()->id)->seller->shopMetrics->first() ?? 0;
+
+        $this->shopMetricsId = $shopMetrics->id;
 
         // dd($shopMetrics->target_sales);
         $this->currentTargetSales = $shopMetrics->target_sales ?? 0;
@@ -34,6 +46,25 @@ class ShopMetricSettings extends Component
         $validator = $this->validate([
             'targetSales' => 'required',
         ]);
-        dd($validator);
+
+        // dd($this->shopMetricsId);
+
+        $shopMetyricsUpdate = SellerShopMetrics::findOrFail($this->shopMetricsId);
+
+        try {
+            $shopMetyricsUpdate->update([
+                'target_sales' => $this->targetSales,
+            ]);
+
+            $this->alert('success', 'Shop Target Sales Metrics Updated', [
+                'position' => 'center',
+                'toast' => false,
+            ]);
+
+            $this->mount();
+
+        } catch (Exception $e) {
+            $this->alert('error', $e->getMessage());
+        }
     }
 }
