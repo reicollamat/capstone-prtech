284fb85d98a78f0f3e8e58a62988db14a871517c
richmondb <richmond.billones@gmail.com>
2023-12-14 23:42:11 +0800

    new sql file of comments 1 and 2 combines with seller_id

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
@@ -14,11 +13,15 @@ class CommentSeeder extends Seeder
      */
     public function run(): void
     {
-        $sql = File::get('database/seeders/sql/comments-1.sql');
+
+        $sql = File::get('database/seeders/sql/commentsof1_2.sql');
         DB::unprepared($sql);
 
-        $sql = File::get('database/seeders/sql/comments-2.sql');
-        DB::unprepared($sql);
+        // $sql = File::get('database/seeders/sql/comments-1.sql');
+        // DB::unprepared($sql);
+        //
+        // $sql = File::get('database/seeders/sql/comments-2.sql');
+        // DB::unprepared($sql);
 
         // $sql = File::get('database/seeders/sql/comments-3.sql');
         // DB::unprepared($sql);
