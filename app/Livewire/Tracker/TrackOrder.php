52aa035dce728a83ca03302682633bab0d0f2b04
richmondb <richmond.billones@gmail.com>
2024-01-13 10:12:29 +0800

    fix for tracking

--- app/Livewire/Tracker/TrackOrder.php
+++ app/Livewire/Tracker/TrackOrder.php
@@ -8,17 +8,6 @@ use Livewire\Component;
 
 class TrackOrder extends Component
 {
-    //    public function placeholder()
-    //    {
-    //        return <<<'HTML'
-    //            <div class="w-full h-full d-flex justify-center items-center x-transition" x-transition.duration.500ms>
-    //                <div class="spinner-grow" style="width: 3rem; height: 3rem;" role="status">
-    //                    <span class="visually-hidden">Loading...</span>
-    //                </div>
-    //            </div>
-    //        HTML;
-    //    }
-
     public $reference;
 
     public $email;
@@ -39,6 +28,7 @@ class TrackOrder extends Component
 
     public function submit()
     {
+        // dd('test');
         // sleep(10);
         $user = User::where('email', $this->email)->first();
 
