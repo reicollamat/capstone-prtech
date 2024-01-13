ce6cf45f920f18363b934e3a0259e189880b2577
richmondb <richmond.billones@gmail.com>
2024-01-13 10:23:05 +0800

    fix for smaller screen but not neeeded

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar padding-remove bg-body-tertiary z-50" role="navigation">
     <div class="container-fluid  bg-dark text-white content-center py-2 z-50">
-        <button class="navbar-toggler outline-none outline-remove py-2 md:hidden" type="button" data-bs-toggle="offcanvas"
-            data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 16" color="white"
-                fill="currentColor">
-                <rect width="30" height="2"></rect>
-                <rect y="7" width="20" height="2"></rect>
-                <rect y="14" width="30" height="2"></rect>
-            </svg>
-        </button>
-        <a class="navbar-brand text-white md:hidden" href="#">PR-Tech Navigation</a>
-        <div class="hidden md:block text-white">
+        {{-- <button class="navbar-toggler outline-none outline-remove py-2 md:hidden" type="button" data-bs-toggle="offcanvas" --}}
+        {{--     data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation"> --}}
+        {{--     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 16" color="white" --}}
+        {{--         fill="currentColor"> --}}
+        {{--         <rect width="30" height="2"></rect> --}}
+        {{--         <rect y="7" width="20" height="2"></rect> --}}
+        {{--         <rect y="14" width="30" height="2"></rect> --}}
+        {{--     </svg> --}}
+        {{-- </button> --}}
+        {{-- <a class="navbar-brand text-white md:hidden" href="#">PR-Tech Navigation</a> --}}
+        <div class="text-white">
             <ul class="nav">
                 <li class="nav-link text-white">
                     <a href="{{ route('index_landing') }}" class="text-white no-underline">
@@ -218,39 +218,39 @@
     </div>
 
     {{-- responsive offcanvas on the side menu --}}
-    <div class="offcanvas offcanvas-start z-50" tabindex="-1" id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('index_landing') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <form class="d-flex mt-3" role="search" >
-                <input class="form-control me-2" type="search" placeholder="Search PR-Tech" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
-    </div>
+    {{-- <div class="offcanvas offcanvas-start z-50" tabindex="-1" id="offcanvasNavbar" --}}
+    {{--     aria-labelledby="offcanvasNavbarLabel"> --}}
+    {{--     <div class="offcanvas-header"> --}}
+    {{--         <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5> --}}
+    {{--         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button> --}}
+    {{--     </div> --}}
+    {{--     <div class="offcanvas-body"> --}}
+    {{--         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3"> --}}
+    {{--             <li class="nav-item"> --}}
+    {{--                 <a class="nav-link active" aria-current="page" href="{{ route('index_landing') }}">Home</a> --}}
+    {{--             </li> --}}
+    {{--             <li class="nav-item"> --}}
+    {{--                 <a class="nav-link" href="#">Link</a> --}}
+    {{--             </li> --}}
+    {{--             <li class="nav-item dropdown"> --}}
+    {{--                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" --}}
+    {{--                     aria-expanded="false"> --}}
+    {{--                     Dropdown --}}
+    {{--                 </a> --}}
+    {{--                 <ul class="dropdown-menu"> --}}
+    {{--                     <li><a class="dropdown-item" href="#">Action</a></li> --}}
+    {{--                     <li><a class="dropdown-item" href="#">Another action</a></li> --}}
+    {{--                     <li> --}}
+    {{--                         <hr class="dropdown-divider"> --}}
+    {{--                     </li> --}}
+    {{--                     <li><a class="dropdown-item" href="#">Something else here</a></li> --}}
+    {{--                 </ul> --}}
+    {{--             </li> --}}
+    {{--         </ul> --}}
+    {{--         <form class="d-flex mt-3" role="search" > --}}
+    {{--             <input class="form-control me-2" type="search" placeholder="Search PR-Tech" aria-label="Search"> --}}
+    {{--             <button class="btn btn-outline-success" type="submit">Search</button> --}}
+    {{--         </form> --}}
+    {{--     </div> --}}
+    {{-- </div> --}}
 </nav>
