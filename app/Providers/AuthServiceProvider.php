36b9623ac2b34f0c3adcbd177044440d74b91617
richmondb <richmond.billones@gmail.com>
2024-01-15 11:01:31 +0800

    customize the email template fo verification

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 use App\Models\User;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
@@ -29,6 +31,16 @@ class AuthServiceProvider extends ServiceProvider
             // dd($user->permissions['platform.index'] == 1);
 
             return $user->permissions['platform.index'] == 1;
+        });
+
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->from('prtech.business@gmail.com', 'PR-TECH')
+                ->greeting('Hi there!')
+                ->subject('Email Verification from PR-TECH')
+                ->line('To start shopping with us, please verify your email by clicking the button down below. Thank you.')
+                ->line('Click the button below to verify your email address.')
+                ->action('Verify Email Address', $url);
         });
     }
 }
