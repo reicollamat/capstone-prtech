ad5667707d0a5e1a2ab5831a7a673886ae82521b
raycollamat <raycollamat11@gmail.com>
2024-04-02 09:45:18 +0800

    test notif scheduling

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,13 @@
 
 namespace App\Console;
 
+use App\Models\Product;
+use App\Models\User;
+use App\Notifications\SellerNotification;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 
 class Kernel extends ConsoleKernel
 {
@@ -13,6 +18,16 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            // 
+            // dd(Auth::user()->seller->products()->whereColumn('stock', '<=', 'reserve')->get());
+            $user = User::where('name', 'kim_pc')->first();
+            $products = $user->seller->products()->whereColumn('stock', '<=', 'reserve')->get();
+
+            foreach ($products as $key => $product) {
+                Notification::send($user, new SellerNotification($product->title));
+            }
+        })->everyMinute();
     }
 
     /**
@@ -20,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
